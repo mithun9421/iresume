f5b7ba406f08bea53a3c0bba817283015b22ffb9
mithun9421 <mithunmuralee94@gmail.com>
2023-01-30 00:21:52 +0530

    enh:changing the env names

--- src/supaClient/index.js
+++ src/supaClient/index.js
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(process.env.SUPABASE_PROJECT_NAME, process.env.SUPABASE_JWT)
+export const supabase = createClient(process.env.REACT_APP_SUPABASE_PROJECT_NAME, process.env.REACT_APP_SUPABASE_JWT)
